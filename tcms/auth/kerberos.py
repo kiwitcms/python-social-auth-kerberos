2d04a7f8dd8b22cc66105e3366f891fb976e4f62
Mr. Senko <atodorov@mrsenko.com>
2018-03-22 14:12:02 +0200

    Add missing imports

--- tcms/auth/kerberos.py
+++ tcms/auth/kerberos.py
@@ -1,5 +1,7 @@
 import kerberos
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend, RemoteUserBackend
 
 
 class KerberosBackend(ModelBackend):

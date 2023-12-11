f35b30ca6ce892b4e6f1784f7816396b3dbc3505
Alexander Todorov <atodorov@otb.bg>
2023-12-11 21:32:42 +0200

    Build and test with Python 3.11

--- setup.py
+++ setup.py
@@ -27,5 +27,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.11",
     ],
 )
